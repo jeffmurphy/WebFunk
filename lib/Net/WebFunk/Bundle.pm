342510e5e7e053c6759f59b9d8caa83f2bab7071
jeff.murphy <jeff.murphy@d2a2b947-c2e8-a591-71fe-6999810ac032>
2011-11-03 19:41:29 +0000

    budnle

---
+++ lib/Net/WebFunk/Bundle.pm
@@ -0,0 +1,32 @@
+package Bundle::WebFunk;
+
+=pod
+
+=head1 CONTENT
+
+ Apache2::Const;
+
+ Apache2::Request;
+
+ Apache2::RequestIO;
+
+ Apache2::RequestRec;
+
+ Apache2::URI;
+
+ Data::Dumper;
+
+ JSON;
+
+ LWP;
+
+ LWP::UserAgent;
+
+ Module::Pluggable;
+
+ Moose;
+
+
+=cut
+
+1;
